559ce643492ec7e11fae7eccca6026fe7675188c
Majed BOUKADIDA <majed.boukadida@gmail.com>
2018-09-29 23:04:14 +0200

    Create new console project to use Polly

---
+++ Polly/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Polly
+{
+    class Program
+    {
+        private static int retryCount = 3;
+        private static readonly TimeSpan delay = TimeSpan.FromSeconds(5);
+
+        static void Main(string[] args)
+        {
+            OperationWithBasicRetryAsync();
+        }
+
+        public static Task OperationWithBasicRetryAsync()
+        {
+            int currentRetry = 0;
+
+            for (; ; )
+            {
+                try
+                {
+                    HttpClient client = new HttpClient() { BaseAddress = new Uri("https://reqres.inn/") };
+                    var response = client.GetAsync("api/users");
+                    Console.WriteLine(response.Result);
+                    break;
+                }
+                catch (AggregateException ex)
+                {
+                    Trace.TraceError("Operation Exception");
+
+                    currentRetry++;
+                    if (currentRetry > retryCount)
+                    {
+                        throw;
+                    }
+                }
+
+                Task.Delay(delay);
+            }
+
+            return Task.FromResult(0);
+        }
+    }
+}
