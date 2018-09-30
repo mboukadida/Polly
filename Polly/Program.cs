f21ac1db1feb26b861436013e6dbc788291c7000
Majed BOUKADIDA <majed.boukadida@gmail.com>
2018-09-30 22:54:34 +0200

    Add fallBackPolicy

--- Polly/Program.cs
+++ Polly/Program.cs
@@ -1,5 +1,4 @@
-﻿using Polly.CircuitBreaker;
-using Polly.Fallback;
+﻿using Polly.Fallback;
 using Polly.Wrap;
 using System;
 using System.Net.Http;
@@ -9,15 +8,29 @@ namespace Polly
 {
     class Program
     {
-        private static Policy waitAndRetryPolicy = Policy
+        private static Policy<HttpResponseMessage> waitAndRetryPolicy = Policy<HttpResponseMessage>
             .Handle<AggregateException>()
             .WaitAndRetry(2, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
 
-        private static Policy circuitBreakerPolicy = Policy
+        private static Policy<HttpResponseMessage> circuitBreakerPolicy = Policy<HttpResponseMessage>
             .Handle<AggregateException>()
             .CircuitBreaker(2, TimeSpan.FromSeconds(5));
 
-        private static PolicyWrap myStrategy = Policy.Wrap(waitAndRetryPolicy, circuitBreakerPolicy);
+        private static FallbackPolicy<HttpResponseMessage> fallBackPolicy = FallbackPolicy<HttpResponseMessage>
+            .Handle<Exception>()
+            .Fallback(
+                fallbackAction: () =>
+                {
+                    Console.WriteLine("Please try again later [Fallback for any exception]");
+                    return new HttpResponseMessage(System.Net.HttpStatusCode.ExpectationFailed);
+                },
+                onFallback: b =>
+                {
+                    ConsoleHelper.WriteLineInColor("Fallback catches failed with: " + b.Exception, ConsoleColor.Red);
+                    Console.ReadLine();
+                });
+
+        private static PolicyWrap<HttpResponseMessage> myStrategy = fallBackPolicy.Wrap(circuitBreakerPolicy.Wrap(waitAndRetryPolicy));
 
         static void Main(string[] args)
         {
