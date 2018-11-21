45e59f58d4cdf999fd6bea0cc13f666bce8fde16
Majed BOUKADIDA <majed.boukadida@gmail.com>
2018-11-21 14:21:25 +0100

    Extract function in new class

--- Polly/Program.cs
+++ Polly/Program.cs
@@ -8,40 +8,9 @@ namespace Polly
 {
     class Program
     {
-        private static Policy<HttpResponseMessage> waitAndRetryPolicy = Policy<HttpResponseMessage>
-            .Handle<AggregateException>()
-            .WaitAndRetry(2, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
-
-        private static Policy<HttpResponseMessage> circuitBreakerPolicy = Policy<HttpResponseMessage>
-            .Handle<AggregateException>()
-            .CircuitBreaker(2, TimeSpan.FromSeconds(5));
-
-        private static FallbackPolicy<HttpResponseMessage> fallBackPolicy = FallbackPolicy<HttpResponseMessage>
-            .Handle<Exception>()
-            .Fallback(
-                fallbackAction: () =>
-                {
-                    Console.WriteLine("Please try again later [Fallback for any exception]");
-                    return new HttpResponseMessage(System.Net.HttpStatusCode.ExpectationFailed);
-                },
-                onFallback: b =>
-                {
-                    ConsoleHelper.WriteLineInColor("Fallback catches failed with: " + b.Exception, ConsoleColor.Red);
-                    Console.ReadLine();
-                });
-
-        private static PolicyWrap<HttpResponseMessage> myStrategy = fallBackPolicy.Wrap(circuitBreakerPolicy.Wrap(waitAndRetryPolicy));
-
         static void Main(string[] args)
         {
-            OperationWithBasicRetryAsync();
-        }
-
-        public static Task OperationWithBasicRetryAsync()
-        {
-            HttpClient client = new HttpClient() { BaseAddress = new Uri("https://reqres.inn/") };
-            var response = myStrategy.Execute(() => client.GetAsync(("api/users")).Result);
-            return Task.FromResult(0);
+            Wrap_Fallback_WaitAndRetry_CircuitBreaker.OperationWithWrappedPolicyAsync().Wait();
         }
     }
 }
---
+++ Polly/Wrap_Fallback_WaitAndRetry_CircuitBreaker.cs
@@ -0,0 +1,49 @@
+﻿using Polly.Fallback;
+using Polly.Wrap;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Polly
+{
+    public class Wrap_Fallback_WaitAndRetry_CircuitBreaker
+    {
+        private static readonly Policy waitAndRetryPolicy = Policy
+           .Handle<Exception>()
+           .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(2)); //TimeSpan.FromSeconds(5)
+
+        private static readonly Policy circuitBreakerPolicy = Policy
+            .Handle<Exception>()
+            .CircuitBreakerAsync(2, TimeSpan.FromSeconds(5));
+
+        private static readonly FallbackPolicy<HttpResponseMessage> fallBackPolicy = Policy<HttpResponseMessage>
+            .Handle<Exception>()
+            .FallbackAsync(
+               fallbackAction: async ct =>
+               {
+                   Console.WriteLine("Please try again later [Fallback for any exception]");
+                   return new HttpResponseMessage(System.Net.HttpStatusCode.ExpectationFailed);
+
+               },
+               onFallbackAsync: async e =>
+               {
+                   ConsoleHelper.WriteInColor("Fallback catches eventually failed with: " + e.Exception.Message, ConsoleColor.Red);
+                   Console.ReadLine();
+               }
+            );
+
+        private static PolicyWrap<HttpResponseMessage> policy = fallBackPolicy
+            .WrapAsync(circuitBreakerPolicy.WrapAsync(waitAndRetryPolicy));
+
+        public static async Task OperationWithWrappedPolicyAsync()
+        {
+            var result = await policy.ExecuteAsync(() => TransientOperationAsync());
+        }
+
+        public static async Task<HttpResponseMessage> TransientOperationAsync()
+        {
+            HttpClient client = new HttpClient() { BaseAddress = new Uri("https://reqres.inn/") };
+            return await client.GetAsync("api/users");
+        }
+    }
+}
