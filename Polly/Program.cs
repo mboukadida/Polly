b30319931e6709a1b7347bbd6438f3fdf64501d9
Majed BOUKADIDA <majed.boukadida@gmail.com>
2018-11-21 13:25:42 +0100

    Extract function in new class

--- Polly/Program.cs
+++ Polly/Program.cs
@@ -6,31 +6,9 @@ namespace Polly
 {
     class Program
     {
-        private static Policy policy = Policy
-            .Handle<Exception>()
-            .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromMilliseconds(10));
-
         static void Main(string[] args)
         {
-            OperationWithBasicRetryAsync().Wait();
-        }
-
-        public static async Task OperationWithBasicRetryAsync()
-		{
-            try
-            {
-                await policy.ExecuteAsync(() => TransientOperationAsync());
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        public static async Task TransientOperationAsync()
-        {
-            HttpClient client = new HttpClient() { BaseAddress = new Uri("https://reqres.inn/") };
-            var response = await client.GetAsync("api/users");
+            WaitAndRetry.OperationWithBasicRetryAsync().Wait();
         }
     }
 }
---
+++ Polly/WaitAndRetry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polly
+{
+    public static class WaitAndRetry
+    {
+        private static Policy policy = Policy
+            .Handle<Exception>()
+            .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(5));
+
+        public static async Task OperationWithBasicRetryAsync()
+        {
+            try
+            {
+                await policy.ExecuteAsync(() => TransientOperationAsync());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static async Task TransientOperationAsync()
+        {
+            HttpClient client = new HttpClient() { BaseAddress = new Uri("https://reqres.inn/") };
+            var response = await client.GetAsync("api/users");
+        }
+    }
+}
